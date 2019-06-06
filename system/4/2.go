26c791b993bbf92a7bfc78e75823a46b5ea32c7b
googege <googege@icloud.com>
2019-06-06 18:14:01 +0800

    add

--- system/4/2.go
+++ system/4/2.go
@@ -1,1 +1,10 @@
 package main
+
+var a = make([]int, 10)
+
+func A(ele int) { // 第一个进去的
+	a = append(a, ele)
+}
+func D() { // 第一个出去就ok了。
+	a = a[:len(a)-1]
+}
--- system/4/4.go
+++ system/4/4.go
@@ -1,1 +1,12 @@
 package main
+
+var a = make([]int, 10)
+
+func R(r int) {
+	a[r]++
+}
+
+func D() {
+	sorts.Ints(a)
+	a = a[1:]
+}
