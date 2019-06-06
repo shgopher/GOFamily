ebbb0b12ef74619bd0fa2e843f5708d1b01afe4c
googege <googege@icloud.com>
2019-06-06 18:46:30 +0800

    add

--- system/4/1.go
+++ system/4/1.go
@@ -1,1 +1,20 @@
 package main
+
+type A struct {
+	next  *A
+	value interface{}
+}
+
+type Box struct {
+	first *A
+	last  *A
+}
+
+func Head(va interface{}) {
+	a := new(Box)
+	value := a.first
+	if value.next.value == va {
+
+	}
+
+}
