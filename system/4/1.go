f254bd75cd9dc57a2421cdfc7e092d99ad002df1
googege <googege@icloud.com>
2019-06-08 01:16:50 +0800

    add

--- system/4/1.go
+++ system/4/1.go
@@ -1,20 +1,3 @@
+//LRU 链表实现方法：
 package main
 
-type A struct {
-	next  *A
-	value interface{}
-}
-
-type Box struct {
-	first *A
-	last  *A
-}
-
-func Head(va interface{}) {
-	a := new(Box)
-	value := a.first
-	if value.next.value == va {
-
-	}
-
-}
