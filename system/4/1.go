b192f3f9d4f2e7a419586ade5b643a168c7303d7
googege <googege@icloud.com>
2019-06-08 01:27:12 +0800

    add

--- system/4/1.go
+++ system/4/1.go
@@ -1,3 +1,35 @@
 //LRU 链表实现方法：
 package main
 
+type Node struct {
+	next  *Node
+	value interface{}
+}
+
+type LinkedList struct {
+	length int
+	head   *Node
+}
+
+
+
+func (l *LinkedList) MoveTOHead(this *Node) {
+	next := this.next
+	now := l.head.next
+	for now.next != nil {
+		if now.next == this {
+			break
+		}
+	}
+	now.next = next
+	this.next = l.head.next
+	l.head.next = this
+}
+
+func (l *LinkedList) Delete() {
+	cur := l.head.next
+	for cur.next.next != nil {
+		cur = cur.next
+	}
+	cur.next = nil
+}
