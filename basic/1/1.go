2ed60382c86e4ce4eb8d389a4dbfc1588315ddcc
googege <googege@icloud.com>
2019-06-07 18:34:36 +0800

    add

--- basic/1/1.go
+++ basic/1/1.go
@@ -1,7 +1,9 @@
 //实现单向,双向，循环链表以及增删操作
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 //单链表（以及单循环链表的节点对象）
 type sNode struct {
@@ -29,7 +31,7 @@ func newLinkedList(ele interface{}) *LinkedList {
 // 新增加一个节点
 func (l *LinkedList) Add(thisNode *sNode, element interface{}) {
 	newOne := new(sNode)
-	newOne.value= element
+	newOne.value = element
 	newOne.next = thisNode.next
 	thisNode.next = newOne
 	l.length++
@@ -87,6 +89,18 @@ func (l *LinkedList) PreSearch(element interface{}) *sNode {
 	return nil
 }
 
+// 按照element查找它的前驱节点
+func (l *LinkedList) prePointer(ele interface{}) *sNode {
+	now := l.head
+	for now.next != nil {
+		if now.next.value == ele {
+			break
+		}
+		now = now.next
+	}
+	return now
+}
+
 // range 出整个的linkedList
 
 func (l *LinkedList) Range() {
@@ -107,14 +121,86 @@ func (l *LinkedList) Range() {
 
 // 双链表 （以及循环双链表的节点对象）
 type dNode struct {
-	prev  *sNode
-	next  *sNode
+	prev  *dNode
+	next  *dNode
 	value interface{}
 }
 
 // 双链表本身
 type LinkedListD struct {
 	head   *dNode
-	trail  *dNode
 	length int
+}
+
+func newListD(ele interface{}) *LinkedListD {
+	return &LinkedListD{
+		head: &dNode{
+			prev:  nil,
+			next:  nil,
+			value: ele,
+		},
+		length: 0,
+	}
+}
+
+// 在任意的节点后增加东西
+func (l *LinkedListD) add(thisNode *dNode, ele interface{}) {
+	newOne := new(dNode)
+	newOne.value = ele
+	next := thisNode.next
+	next.prev = newOne
+	thisNode.next = newOne
+	l.length++
+
+}
+
+// 在任意的节点前增加东西
+func (l *LinkedListD) addP(thisNode *dNode, ele interface{}) {
+	newOne := new(dNode)
+	newOne.value = ele
+	pre := thisNode.prev
+	pre.next = newOne
+	thisNode.prev = newOne
+}
+
+func (l *LinkedListD) addTrail(ele interface{}) {
+	now := l.head
+	for now.next != nil {
+		now = now.next
+	}
+	newOne := new(dNode)
+	newOne.value = ele
+	now.next = newOne
+	newOne.prev = now
+}
+
+// 删除节点
+func (l *LinkedListD) Delete(thisNode *dNode) {
+	preNode := thisNode.prev
+	preNode.next = thisNode.next
+	thisNode.next.prev = preNode
+	l.length--
+}
+
+func (l *LinkedListD) Range() {
+	now := l.head
+	for now.next != nil {
+		fmt.Println(now.value)
+		now = now.next
+	}
+	fmt.Println(now.value)
+}
+
+func (l *LinkedListD) Search(ele interface{}) *dNode {
+	now := l.head
+	for now.next != nil {
+		if now.value == ele {
+			break
+		}
+		now = now.next
+	}
+	if now.value != ele {
+		return nil
+	}
+	return now
 }
--- basic/1/1_test.go
+++ basic/1/1_test.go
@@ -5,12 +5,25 @@ import (
 )
 
 var list = newLinkedList(0)
+
 func TestRange(t *testing.T) {
 	list.AddTrail(12)
 	list.AddTrail(12222)
 	list.Range()
 	list.Delete(list.Search((1)))
 	list.Range()
-	list.Add(list.Search(0),"哈哈哈哈")
+	list.Add(list.Search(0), "哈哈哈哈")
 	list.Range()
+}
+
+var listD = newListD(00)
+
+func TestLinkedList_Range(t *testing.T) {
+
+	listD.addTrail(1)
+	listD.addTrail(2)
+	listD.addTrail(3)
+	listD.Range()
+	listD.Delete(listD.Search(1))
+	listD.Range()
 }
