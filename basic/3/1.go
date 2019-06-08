85580f2900ecda8d3b1b3aa8c625bd2b6a9475de
googege <googege@icloud.com>
2019-06-09 01:33:36 +0800

    add

--- basic/3/1.go
+++ basic/3/1.go
@@ -6,7 +6,8 @@ func main() {
 
 type Stack struct {
 	data []interface{}
-	top  int //表示栈顶
+	top  int //表示栈顶 因为slie会自动扩容，而且需要减少不必要的损失（下文的使用top不直接变slice）所以我们使用top表示栈最后一个，最右边的那个，也可以说是
+	// 最上面一个，其实怎么说都可以 我就说最上面那个吧。反正就是最新的那个 它始终是top，它最先出去。
 }
 
 //放入
