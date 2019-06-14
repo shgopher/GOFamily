1e3cd43c0adb86ac5c9ff76ad982e3843f7197dc
googege <googege@icloud.com>
2019-06-14 21:53:01 +0800

    ad

--- basic/20/1.go
+++ basic/20/1.go
@@ -1,1 +1,18 @@
 package main
+
+// 1 使用链表来实现栈
+// 2 使用数组（切片）来实现栈
+
+// 总之只要满足 FILO就ok了。
+
+//首先我们要实现 栈的 出栈和入栈，以及查阅栈顶这三个功能，然后我将使用两种方式进行选择，然后我们再实现 前进和后退这个功能
+
+// 
+type stackSlice struct {
+}
+
+type stackLinkedList struct {
+}
+
+type Bower struct {
+}
