161cabd9ed07026ad8afb65d88f8b841ebef419c
googege <googege@icloud.com>
2019-06-18 15:53:08 +0800

    add

--- basic/20/1.go
+++ basic/20/1.go
@@ -41,20 +41,35 @@ func Back(b Bower) {
 }
 
 // new 一个新的SatckSlice
-func NewSlice() *StackSlice {
-	return &StackSlice{}
+func NewSlice(c int) *StackSlice {
+	return &StackSlice{
+		length: 0,
+		value:  make([]interface{}, c, c), // 设置应有的容量大小。
+	}
 }
 
 // new 一个新的StackLinkedList
 func NewList() *StackLinkedList {
-	return &StackLinkedList{}
+	return &StackLinkedList{
+		length: 0,
+		head: &node{ // 第一个是哨兵，使用哨兵机制。
+			value: 0,
+			next:  nil,
+		},
+	}
 }
 
 // 数组类型实现栈
 func (sli *StackSlice) In() {
+	sli.length++
 
 }
 func (sli *StackSlice) Out() {
+	if sli.length > 0 {
+		sli.length--
+	} else {
+		sli.length = 0
+	}
 
 }
 func (sli *StackSlice) Next() {
