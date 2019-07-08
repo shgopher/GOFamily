6f41fc8bbe83052dfea10fa46d82504449a7ee1f
googege <googege@icloud.com>
2019-07-09 00:05:15 +0800

    增加对比

--- basic/12/2_test.go
+++ basic/12/2_test.go
@@ -6,7 +6,7 @@ import (
 )
 //0
 func TestT(t *testing.T) {
-	fmt.Println(T(40))
+	fmt.Println(T(40000))
 }
 
 //0.55s
@@ -16,15 +16,17 @@ func TestT1(t *testing.T) {
 //12.9 ns/op
 func BenchmarkT(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		T(10)
+		T(5000)
 	}
 }
 //290 ns/op
 func BenchmarkT1(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		T1(10)
+		T1(50)
 	}
 }
 
 
 // 不重复运算和重复运算的 递归对比，相差了 可以说是将近 30倍。
+
+// 如果是 5000这个数量级 非重复计算的 只有21 但是 重复计算的  肯定是 瘫痪了，这就是 算法 和数据结构的魅力 啊 !!!!
