6e44f66d5d3d1426f98500c4de63a98fe80048c6
googege <googege@icloud.com>
2019-07-08 11:48:34 +0800

    dele

--- basic/3/1.go
+++ basic/3/1.go
@@ -1,30 +1,15 @@
 //使用数组
+// 要实现的功能 包括 1 顺序入出 2 循环式入出 3 阻塞式 4 并发式（包括了两种 原子操作和加锁）
 package main
 
-type Queue interface {
-	// 进去，进队列
-	In(v interface{})
-	// 出队列
-	Out() (v interface{})
-}
+import "sync"
 
 // 使用循环slice来实现一个队列
 type QueueSlice struct {
+	lock sync.Mutex
 	trail  int
 	head   int
 	length int
 	value  []interface{}
 }
 
-// 使用循环链表即可
-type QueueLinkedList struct {
-	trail  *node
-	length int
-	head   *node
-}
-
-type node struct {
-	value interface{}
-	next  *node
-	last  *node
-}
--- basic/3/2.go
+++ basic/3/2.go
@@ -1,2 +1,28 @@
 // 使用链表
+// 要实现的功能 包括 1 顺序入出 2 循环式入出 3 阻塞式 4 并发式（包括了两种 原子操作和加锁）
 package main
+
+import "sync"
+
+// 使用 数组和链表 公用的一个interface。
+type Queue interface {
+	// 进去，进队列
+	In(v interface{})
+	// 出队列
+	Out() (v interface{})
+}
+
+// 链表
+type QueueLinkedList struct {
+	lock sync.Mutex
+	trail  *node
+	length int
+	head   *node
+}
+
+type node struct {
+	value interface{}
+	next  *node
+	last  *node
+}
+
