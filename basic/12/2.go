da29ee5e87618243c3492c2a5783733d39fc721d
googege <googege@icloud.com>
2019-07-09 00:00:50 +0800

    递归 要记得两件事 1 overflow 2 不要重复计算

--- basic/12/2.go
+++ basic/12/2.go
@@ -10,9 +10,21 @@ func T(x int) int {
 		return 1
 	} else if x == 2 {
 		return 2
-	} else if v,ok := e[x]; ok {
+	} else if v, ok := e[x]; ok {
 		return v
 	}
-	e[x] = T(x) // 这里引起了Stack Overflow
-	return T(x-1) + T(x-2)
+	//e[x] = T(x) // 这里引起了Stack Overflow
+	t := T(x-1) + T(x-2)
+	e[x] = t
+	return t
+}
+
+func T1(x int) int {
+	if x == 1 {
+		return 1
+	} else if x == 2 {
+		return 2
+	}
+	return T1(x-1) + T1(x-2)
+
 }
--- basic/12/2_test.go
+++ basic/12/2_test.go
@@ -1,7 +1,30 @@
 package main
 
-import "testing"
-
+import (
+	"fmt"
+	"testing"
+)
+//0
 func TestT(t *testing.T) {
-	T(10)
+	fmt.Println(T(40))
 }
+
+//0.55s
+func TestT1(t *testing.T) {
+	fmt.Println(T1(40))
+}
+//12.9 ns/op
+func BenchmarkT(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		T(10)
+	}
+}
+//290 ns/op
+func BenchmarkT1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		T1(10)
+	}
+}
+
+
+// 不重复运算和重复运算的 递归对比，相差了 可以说是将近 30倍。
