e5d06cb8b7c6dc078009c2d3988918c73d185e32
googege <googege@icloud.com>
2019-07-09 11:22:18 +0800

    add

--- basic/7/1.go
+++ basic/7/1.go
@@ -1,1 +1,14 @@
 package main
+
+//
+
+func Mao(x ...int)[]int{
+	for i := 0;i < len(x);i++ {
+		for j := 0;j < len(x) - i -1  ;j++  {
+			if x[j] > x[j+1] {
+				x[j],x[j+1] = x[j+1],x[j]
+			}
+		}
+	}
+	return x
+}
--- basic/7/1_test.go
+++ basic/7/1_test.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMao(t *testing.T) {
+	fmt.Println("common",Mao(2,4,6566,66,344343,435,663,44,45,5,6,66,65,45))
+	fmt.Println("0",Mao(0,0,0,0,0))
+}
+
+func BenchmarkMao(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Mao(1,32,23,23,2344,5534,23,1,1,323,323,22,55,13,67,7442,24245,5424,)
+	}
+}
+
+func BenchmarkMao1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Mao(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,)
+	}
+}
