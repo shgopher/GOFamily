dad087e66874f6e6816d3ae30e4739e721ca235d
googege <googege@icloud.com>
2019-07-11 09:22:58 +0800

    add

--- basic/7/2.go
+++ basic/7/2.go
@@ -1,11 +1,11 @@
 // 快排 归并 希尔排序
 package main
 
-func XiEr(x ...int)[]int {
+func XiEr(x ...int) []int {
 	var r []int
-	for i := 0;i < len(x);i = i+10{
+	for i := 0; i < len(x); i = i + 10 {
 		r = Cha(x[0:i]...)
-		if i < len(x) && len(x)- i < 10 {
+		if i < len(x) && len(x)-i < 10 {
 			r = Cha(x...)
 		}
 	}
@@ -32,4 +32,42 @@ func ShellSort(arr []int) []int {
 		gap = gap / 3
 	}
 	return arr
+}
+
+// 归并排序
+
+func mergeSort(arr []int) []int {
+	length := len(arr)
+	if length < 2 { // 当这个数组中的数据已经只有一个的时候，就可以返回了。
+		return arr
+	}
+	middle := length / 2                            // 从中间劈开 分治的思想，一直将这些数据从中间劈开。
+	left := arr[0:middle]                           // 从中间劈开的左侧的数据
+	right := arr[middle:]                           // 右侧的数据
+	return merge(mergeSort(left), mergeSort(right)) // 合并 左侧的和右侧的数据，将左右两侧的数据开始 排序。
+}
+
+func merge(left []int, right []int) []int {
+	var result []int
+	for len(left) != 0 && len(right) != 0 {
+		if left[0] <= right[0] {
+			result = append(result, left[0])
+			left = left[1:]
+		} else {
+			result = append(result, right[0])
+			right = right[1:]
+		}
+	}
+
+	for len(left) != 0 {
+		result = append(result, left[0])
+		left = left[1:]
+	}
+
+	for len(right) != 0 {
+		result = append(result, right[0])
+		right = right[1:]
+	}
+
+	return result
 }
