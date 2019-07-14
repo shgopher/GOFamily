74469e853325526e3f7d37c8fcd33eef014acf08
googege <googege@icloud.com>
2019-07-14 22:04:14 +0800

    add

--- al/29/1.go
+++ al/29/1.go
@@ -1,39 +1,34 @@
 package TN
 
-import (
-	"fmt"
-)
-
 // 快排
-func TOPKQuickSelect(x []int, k int) int {
+func TOPKQuickSelect(x []int, k int) {
 	left := 0
 	right := len(x) - 1
-	t := quickSort(left, right, x, k)
-	fmt.Println("测试TOP", t)
-	return t
+	quickSort(left, right, x, k)
+
 }
 
-func quickSort(left int, right int, arr []int, k int) int {
+func quickSort(left int, right int, arr []int, k int) {
 	if left < right {
+		b1, b2 := 0, 0
 		t, m := pattion(left, right, arr, k)
-		fmt.Println("测试pattion", t, m)
-		if t == -1 {
-			return -1
-		} else if t == 0 {
-			return m
-		} else if t == 1 {
-			quickSort(left, m-1, arr, k)
-		} else if t == 2 {
-			quickSort(m+1, right, arr, k)
+		if t == 0 {
+			// fmt.Println(arr[m])
+			return
 		}
+		if t == 1 {
+			b1, b2 = left, m-1
+		} else if t == 2 {
+			b1, b2 = m+1, right
+		}
+		quickSort(b1, b2, arr, k)
 	}
-	return -110
 }
 func pattion(left int, right int, arr []int, k int) (int, int) {
 	p := left
 	index := p + 1
 	for i := index; i <= right; i++ {
-		if arr[i] < arr[p] {
+		if arr[i] > arr[p] {
 			arr[i], arr[index] = arr[index], arr[i]
 			index += 1
 		}
@@ -41,7 +36,7 @@ func pattion(left int, right int, arr []int, k int) (int, int) {
 	arr[p], arr[index-1] = arr[index-1], arr[p]
 	if k == index {
 		return 0, index - 1
-	} else if k > index {
+	} else if k < index {
 		return 1, index - 1
 	} else {
 		return 2, index - 1
