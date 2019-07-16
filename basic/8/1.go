e59b684e8e721c879b8ef82f60058bd219dabb44
googege <googege@icloud.com>
2019-07-16 08:33:54 +0800

    add

--- basic/8/1.go
+++ basic/8/1.go
@@ -18,6 +18,32 @@ func ER(x []int, value int) int {
 	return -1
 }
 
+// 	low := 0
+// heigh := len(x)
+// 使用递归的方法来实现查找。
+func ERD(x []int, value int) int {
+	low := 0
+	heigh := len(x) - 1
+	return eRD(x, low, heigh, value)
+}
+func eRD(x []int, low int, heigh int, value int) int {
+	middle := low + (heigh-low)>>1
+	result := 0
+	if low <= heigh {
+
+		if value == x[middle] {
+			result = middle
+		} else if value > x[middle] {
+			result = eRD(x, middle+1, heigh, value)
+		} else {
+			result = eRD(x, low, middle-1, value)
+		}
+
+	}
+	return result
+
+}
+
 func main() {
 	fmt.Println(ER([]int{1, 45, 66, 777, 8999}, 45))
 }
