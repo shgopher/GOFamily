247512b5887ce07b47dc02cb05f4306c1ac0ea61
googege <googege@icloud.com>
2019-07-22 09:36:46 +0800

    add

--- basic/25/1.go
+++ basic/25/1.go
@@ -36,3 +36,53 @@ func Search1(root *BinarySearchTree, x int) *BinarySearchTree {
 	return root
 
 }
+
+// 数据的插入
+// 返回插入的那个节点的指针
+func Insert(root *BinarySearchTree, value int) *BinarySearchTree {
+	var result *BinarySearchTree
+
+	// 如果二叉查收树是空的，那么就创建一个根树
+	if root == nil {
+		a := new(BinarySearchTree)
+		a.value = value
+		root = a
+		return root
+	}
+
+	// 如果要创建的数据跟原有的数据一样大，不好意思我们无法帮你插入，因为这是不符合规定的，抱歉了。
+	if value == root.value {
+		return &BinarySearchTree{
+			value: -1,
+		}
+	}
+	// 如果数据比根节点大
+	if value > root.value {
+		// 如果右边是空的，直接嫁接到右边即可
+		if root.right == nil {
+			a := new(BinarySearchTree)
+			a.value = value
+			root.right = a
+			return a
+			//如果不是空的，那么请递归吧，直到找到是空的位置。
+		} else {
+			result = Insert(root.right, value)
+		}
+		// 这个就是数据比root小
+	} else {
+		// 如果left是空的，那么请嫁接上吧
+		if root.left == nil {
+			a := new(BinarySearchTree)
+			a.value = value
+			root.left = a
+			return a
+			// 有数据?请递归吧。
+		} else {
+			// 返回递归的数据，如果不返回最高的一层也拿不到数据啊。
+			result = Insert(root.left, value)
+		}
+
+	}
+	// 返回数据
+	return result
+}
--- basic/25/1_test.go
+++ basic/25/1_test.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+var (
+	b = new(BinarySearchTree)
+)
+
+func TestInsert(t *testing.T) {
+	// 测试会不会生成root
+	a1 := Insert(b, 1)
+	fmt.Println("测试a1", *a1)
+	// 测试普通的插入
+	a11 := Insert(b, 2)
+	a12 := Insert(b, 3)
+	a13 := Insert(b, 4)
+	a14 := Insert(b, 5)
+	a2 := Insert(b, 6)
+	fmt.Println("测试a2", *a2, *a11, *a12, *a13, *a14)
+	// 测试是否会返回 -1
+	a3 := Insert(b, 6)
+	fmt.Println("测试a3", *a3)
+}
+
+func TestSearch(t *testing.T) {
+	t1 := Search(b, 3)
+	fmt.Println("测试search", *t1)
+}
