ed2e96db006fabe654867a21f7ac4e24b871c8a3
googege <googege@icloud.com>
2019-07-22 17:41:03 +0800

    add

--- basic/25/2.go
+++ basic/25/2.go
@@ -28,19 +28,19 @@ func InsertM(b *Box, value int, k2 string) {
 			{k1: value, k2: k2},
 		}
 	}
-	for {
+	for { // 循环的额目的是为了找到left或者nil等于nil的地方
 		if value > b.value[0].k1 {
-			if b.right == nil {
+			if b.right == nil { // 如果等于nil那么我们只需要给他一个新的node节点即可。
 				n := new(Box)
 				n.value = []*node{
 					{k1: value, k2: k2},
 				}
 				b.right = n
-				break
+				break // 然后break出来
 			} else {
-				b = b.right
+				b = b.right // 如果下一个节点不是nil那么我们应该找下个节点。
 			}
-		} else if value < b.value[0].k1 {
+		} else if value < b.value[0].k1 { // 这里为什么可以直接使用value[0] 因为 不管是root是nil的情况(我们给他了0值)还是下一步为nil的状态如果是nil我们给了它新的node，如果不是nil那么它一定是拥有数组的。所以我们可以随意使用[0]
 			if b.left == nil {
 				n := new(Box)
 				n.value = []*node{
