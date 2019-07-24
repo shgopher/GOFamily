3855da54c7604d1c90c0d22b9eb3c92afc4a1c07
googege <googege@icloud.com>
2019-07-25 01:08:19 +0800

    add

--- basic/5/2.go
+++ basic/5/2.go
@@ -1,2 +1,32 @@
 // 堆排序
 package main
+
+func topToDownPaixu(h *Heap, i int) {
+	for {
+		max := i
+		if 2*i <= h.count && h.value[i] < h.value[2*i] {
+			max = 2 * i
+		}
+
+		if 2*i+1 <= h.count && h.value[max] < h.value[2*i+1] {
+			max = 2*i + 1
+		}
+		if max == i {
+			break
+		}
+		h.value[i], h.value[max] = h.value[max], h.value[i]
+		i = max
+	}
+
+}
+func pai(a []int) []int {
+
+	h := NewHeap(10)
+	h.value = a
+	h.count = len(a) - 1
+	for i := h.count / 2; i >= 1; i-- {
+		topToDownPaixu(h, i)
+	}
+	return h.value
+
+}
--- basic/5/2_test.go
+++ basic/5/2_test.go
@@ -1,1 +1,10 @@
 package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestPait(t *testing.T) {
+	fmt.Println(pai([]int{0, 1, 3, 4, 5, 6, 2, 89, 87, 54}))
+}
