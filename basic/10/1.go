dcd23ac725a2239144e8c350af305850d6cfcac6
googege <googege@icloud.com>
2019-07-31 22:35:29 +0800

    add

--- basic/10/1.go
+++ basic/10/1.go
@@ -1,7 +1,12 @@
 // 广度优先搜索
 package main
 
-import "container/list"
+import (
+	"container/list"
+	"fmt"
+)
+
+// a表示启示顶点 b表示终止顶点，搜索算法查找的是ab两个顶点之间的路径
 
 // adj -- adjacency table 邻接表
 type Graph struct {
@@ -29,11 +34,24 @@ func (g *Graph) AddEdge(a, b int) {
 }
 
 // 广度优先搜索
-func (g *Graph) BFS() {
+func (g *Graph) BFS(a, b int) {
+	if a == b {
+		return
+	}
 
 }
 
 // 深度优先搜索
 func (g *Graph) DFS() {
 
+}
+
+// 遍历搜索路径
+func (g *Graph) Range(pre []int, a, b int) {
+	if a == b || pre[b] == -1 {
+		fmt.Println(b)
+	} else {
+		g.Range(pre, a, pre[b]) // 往前找路径。递归到a == b 或者 pre[b] = -1 就会跳出来。
+		fmt.Println(b)
+	}
 }
