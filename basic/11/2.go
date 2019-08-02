f6e2fd37822ea20c2c27789421aaccf441af078d
googege <googege@icloud.com>
2019-08-02 11:02:18 +0800

    add

--- basic/11/2.go
+++ basic/11/2.go
@@ -1,1 +1,72 @@
 package main
+
+// RK 就是再增加一个 哈希算法，将所有的字符串转化为一个一个的哈希值，这样直接进行比较就很快了。
+var (
+	ttt map[string]int
+)
+
+func bt(ttt map[string]int) {
+	ttt["a"] = 1
+	ttt["b"] = 2
+	ttt["c"] = 3
+	ttt["d"] = 4
+	ttt["e"] = 5
+	ttt["f"] = 6
+	ttt["g"] = 7
+	ttt["h"] = 8
+	ttt["i"] = 9
+	ttt["j"] = 10
+	ttt["k"] = 11
+	ttt["l"] = 12
+	ttt["m"] = 13
+	ttt["n"] = 14
+	ttt["o"] = 15
+	ttt["p"] = 16
+	ttt["q"] = 17
+	ttt["r"] = 18
+	ttt["s"] = 19
+	ttt["t"] = 20
+	ttt["u"] = 21
+	ttt["v"] = 22
+	ttt["w"] = 23
+	ttt["x"] = 24
+	ttt["y"] = 25
+	ttt["z"] = 26
+}
+func Hash(a string) int {
+	tii := make(map[string]int)
+	bt(tii)
+	ary := len(a) // 表示进制
+	var result int
+	for i := 0; i < len(a); i++ {
+		b := tii[string(a[i])]
+		result += b * exponent(ary, i)
+	}
+	return result
+}
+
+func exponent(a, n int) int {
+	result := int(1)
+	for i := n; i > 0; i >>= 1 {
+		if i&1 != 0 {
+			result *= a
+		}
+		a *= a
+	}
+	return result
+}
+
+func Pp(m, p string) bool {
+	for i := 0; i <= len(m)-len(p); i++ {
+		var a int
+		if i+len(p) >= len(m) {
+			a = Hash(string(m[i:]))
+		} else {
+			a = Hash(string(m[i : i+len(p)]))
+		}
+		if a == Hash(p) {
+			return true
+		}
+	}
+	return false
+}
--- basic/11/2_test.go
+++ basic/11/2_test.go
@@ -1,3 +1,34 @@
 package main
 
-// RK 就是再增加一个 哈希算法，将所有的字符串转化为一个一个的哈希值，这样直接进行比较就很快了。
+import (
+	"fmt"
+	"testing"
+)
+
+func TestPp(t *testing.T) {
+	fmt.Println("测试hash", Pp("abcdefghijklmnopqrstuvwxyz", "abcde")) // 头
+	fmt.Println("测试hash", Pp("abcdefghijklmnopqrstuvwxyz", "wxyz"))  // 尾
+	fmt.Println("测试hash", Pp("abcdefghijklmnopqrstuvwxyz", "jklm"))  // 普通
+	fmt.Println("测试hash", Pp("abcdefghijklmnopqrstuvwxyz", "bblm"))  // 没有
+	// true
+}
+
+func BenchmarkPp(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Pp("abcdefghijklmnopqrstuvwxyz", "foew")
+	}
+}
+
+func BenchmarkPp1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Pp("abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz", "foew")
+	}
+}
+
+func BenchmarkPp2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Pp("abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz", "foew")
+	}
+}
+
+// todo 哈希算法要优化，目前性能很糟糕。
