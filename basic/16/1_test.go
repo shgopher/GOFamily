65f4014be3c171f1d691c566124e02f81f45ad88
googege <googege@icloud.com>
2019-08-10 17:31:06 +0800

    add

--- basic/16/1_test.go
+++ basic/16/1_test.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestDPPackage(t *testing.T) {
+	ZW = 12
+	a = []int{1, 2, 3, 4, 5}
+	result = 0
+	status := make([][]int, len(a))
+	for i := 0; i < len(status); i++ {
+		status[i] = make([]int, ZW+1)
+	}
+	DPPackage(ZW, a, &result, status)
+	fmt.Println(result)
+}
