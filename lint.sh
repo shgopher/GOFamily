c66a76e5df7047153f9961230050dbefac08cc9b
shgopher <shgopher@gmail.com>
2023-11-22 14:12:22 +0800

    feat(root): update lint.sh

--- lint.sh
+++ lint.sh
@@ -1,43 +1,50 @@
-#!/bin/bash
+#!/bin/zsh
 ###
  # @Author: shgopher shgopher@gmail.com
  # @Date: 2023-11-16 14:31:20
  # @LastEditors: shgopher shgopher@gmail.com
- # @LastEditTime: 2023-11-16 14:40:54
+ # @LastEditTime: 2023-11-22 14:09:17
  # @FilePath: /GOFamily/lint.sh
  # @Description: 
  # 
  # Copyright (c) 2023 by shgopher, All Rights Reserved. 
 ### 
 
+
 find_md(){
 
   local printed_files=()
 
-  for item in $(ls -R "$1")
-  do
-    if [[ "$item" == *.md ]]; then
+  # 检查 zhlint 命令是否存在  
+  if ! [ -x "$(command -v zhlint)" ]; then
 
+    # 安装 zhlint
+    npm install -g zhlint > /dev/null 
+  fi
+
+  for item in $(ls -R "$1"); do
+
+    if [[ "$item" == *.md ]]; then
+      
       path="$1/$item"
 
       if [[ ! " ${printed_files[@]} " =~ " $path " ]]; then
-        
+      
         echo "$path"
-        
-        # 调用zhlint修复格式
-        zhlint "$path" --fix
-        
+
+        # 调用zhlint格式化    
+        zhlint "$path" --fix > /dev/null
+      
         printed_files+=("$path")
       fi
 
     elif [[ -d "$1/$item" ]]; then
 
-      find_md "$1/$item"
-
+      find_md "$1/$item" 
     fi
 
-  done
-
+  done  
 }
 
+# 调用 
 find_md "$(pwd)"
